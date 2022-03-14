28202fc081db732d8e7434b40e21e97784893176
Okabe Rintarou <eliotandelon@gmail.com>
2022-03-13 23:56:48 -0500

    preprocessor directive

--- CTutorials/code_files/src/mathematic_functions.h
+++ CTutorials/code_files/src/mathematic_functions.h
@@ -1,3 +1,6 @@
+#ifndef MATHEMATIC_FUNCTIONS_H
+#define MATHEMATIC_FUNCTIONS_H
+
 int add(int num, int num2);
 
 int substract(int num, int num2);
@@ -5,3 +8,5 @@ int substract(int num, int num2);
 int multiply(int num, int num2);
 
 float divide(float num, float num2);
+
+#endif // MATHEMATIC_FUNCTIONS_H
